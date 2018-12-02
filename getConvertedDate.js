28eb9c052033a0240fdd37f0bea769ddfb2f5163
Aliaksandr Nazaau <aliaksandr.nazarov@gmail.com>
2018-12-02 12:51:18 -0700

    Add comment.

--- getConvertedDate.js
+++ getConvertedDate.js
@@ -1,3 +1,7 @@
+// this function convert date or string to specified timezone in mins (for example for Minsk it is -180)
+// by default used GMT +3(-180) , Minsk timezone
+// standart method toIsoString works wrong, do not use!
+
 const getConvertedDate = (value, medcenterOffset = (-180)) => {
   const today = value ? new Date(value) : new Date();
   const timeInMs = today.getTime();
